6500560cc6cee7acd6b3894e01e9ffbbda8fb0a7
Pedro Perafán <pedro_inat18@hotmail.com>
2021-04-28 23:54:24 -0500

    eslint

--- test/stimulus-test.js
+++ test/stimulus-test.js
@@ -2,30 +2,29 @@ import { Application } from 'stimulus'
 import pretty from 'pretty'
 
 export default class {
-    static #nextFrame() {
-        return new Promise(resolve => window.requestAnimationFrame(resolve))
+  static #nextFrame() {
+    return new Promise((resolve) => window.requestAnimationFrame(resolve))
+  }
+
+  static #findControllerName(html) {
+    const matches = html.match(/data-controller="(.*)"/)
+
+    if (matches) {
+      return matches[1]
     }
 
-    static #findControllerName(html) {
-        const matches = html.match(/data-controller="(.*)"/)
+    throw new Error('Couldn\'t find data-controller attribute')
+  }
 
-        if (matches) {
-            return matches[1]
-        }
+  static stimulusFixture(html, controllerClass) {
+    const name = this.#findControllerName(html)
 
-        throw new Error('Couldn\'t find data-controller attribute')
-    }
+    document.body.outerHTML = html
+    const application = Application.start(document.body)
+    application.register(name, controllerClass)
 
-    static stimulusFixture(html, controllerClass) {
-        const name = this.#findControllerName(html)
-
-        document.body.outerHTML = html;
-        const application = Application.start(document.body)
-        application.register(name, controllerClass)
-
-        return this.#nextFrame()
-    }
+    return this.#nextFrame()
+  }
 
     static documentHtml = () => pretty(document.body.innerHTML)
 }
-
